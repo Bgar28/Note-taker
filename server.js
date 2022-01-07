8b453d0201a64e8d2d87b0a8c4378c6cf3ff0941
Brian Garland <briangarlandk@gmail.com>
2022-01-06 20:28:30 -0600

    Required libraries

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+let db = require('./db/db.json');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
