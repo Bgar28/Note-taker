08602cee1e801df0eaa3acfca1adfb6614520f95
Brian Garland <briangarlandk@gmail.com>
2022-01-09 14:36:37 -0600

    Added error handling for post and delete route

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const res = require('express/lib/response');
 const fs = require('fs');
 const path = require('path');
 let db = require('./db/db.json');
@@ -14,19 +13,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // GET route for homepage
-app.get('/', (req, res) =>
+app.get('/', (req, res) => 
     res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 // GET route for notes page
-app.get('/notes', (req, res) =>
+app.get('/notes', (req, res) => 
     res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
 // GET route to read and return db.json file as JSON
-app.get('/api/notes', (req, res) =>
+app.get('/api/notes', (req, res) => {
     res.json(db)
-);
+});
 
 // POST route will save new note, add to db.json file, then return it to client
 app.post('/api/notes', (req, res) => {
@@ -37,17 +36,24 @@ app.post('/api/notes', (req, res) => {
         id: uuid(),
     };
     db.push(newNote)
-    fs.writeFile('./db/db.json', JSON.stringify(db), () => {
-        console.log('db is here')
-        res.json(newNote)
+    fs.writeFile('./db/db.json', JSON.stringify(db), (err) => {
+        if (err) {
+           return res.status(400).json(err);
+        }
+        console.log('db is here');
+        res.json(newNote);
     })
 });
 
+// DELETE route will delete a note with specified id 
 app.delete('/api/notes/:id', (req, res) => {
     const updatedDb = db.filter((note) => {
-        return note.id != req.params.id
+        return note.id !== req.params.id
     })
-    fs.writeFile('./db/db.json', JSON.stringify(updatedDb), () => {
+    fs.writeFile('./db/db.json', JSON.stringify(updatedDb), (err) => {
+        if (err) {
+            return res.status(400).json(err);
+         }
         console.log('db is here')
         res.json(updatedDb)
     })
